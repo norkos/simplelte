db37819b1d0c116d0f6f3fe3003edb33259442e9
norkos <norbert.kosmal@gmail.com>
2016-07-03 14:22:00 +0200

    UTs for MessageHandler

--- eNB/include/utils/MessageHandler.hpp
+++ eNB/include/utils/MessageHandler.hpp
@@ -4,7 +4,33 @@
 #include <unordered_map>
 #include <typeindex>
 
-#include "HandlerFunction.hpp"
+template<class MessageBaseT>
+class HandlerFunctionBase{
+public:
+    virtual ~HandlerFunctionBase() {}
+    virtual void execute(const MessageBaseT&) = 0;
+};
+
+template<class T, class MessageBaseT, class MessageT>
+class HandlerFunction : public HandlerFunctionBase<MessageBaseT>{
+
+typedef void(T::*MemFun)(const MessageT&);
+
+public:
+        
+    HandlerFunction(T* source, MemFun fun): 
+        source_(source), function_(fun){ }
+    
+    virtual ~HandlerFunction() { }
+    
+    virtual void execute(const MessageBaseT& msg){
+        (source_->*function_)(static_cast<const MessageT&>(msg));
+    }
+
+private:
+    T* source_;
+    MemFun function_;
+};
 
 template<class MessageBaseT>
 class MessageHandler{
@@ -12,22 +38,25 @@ class MessageHandler{
 typedef std::unordered_map<std::type_index, HandlerFunctionBase<MessageBaseT> *> Handler;
     
 public:
-    ~MessageHandler(){}
+    virtual ~MessageHandler(){}
     
-    void handleMessage(const MessageBaseT& msg){
-        typename Handler::iterator it = handlers.find(typeid(msg));
-        if(it != handlers.end()){
+    bool handleMessage(const MessageBaseT& msg){
+        typename Handler::iterator it = handler_.find(typeid(msg));
+        
+        if(it != handler_.end()){
             it->second->execute(msg);
+            return true;
         }
+        return false;
     }
     
     template<class T, class MessageT>
     void registerMessage(T* source, void (T::*memFun)(const MessageT&)){
-        handlers[typeid(MessageT)] = new HandlerFunction<T, MessageT, MessageBaseT>(source, memFun);
+        handler_[typeid(MessageT)] = new HandlerFunction<T, MessageBaseT, MessageT>(source, memFun);
     }
      
 private:
-    Handler handlers;
+    Handler handler_;
 };
 
 #endif
--- eNB/ut/utils/MessageHandlerUT.cpp
+++ eNB/ut/utils/MessageHandlerUT.cpp
@@ -1,7 +1,77 @@
 #include <gtest/gtest.h>
+#include <memory>
+
+#include "utils/MessageHandler.hpp"
 
 namespace GT = ::testing;
 
-TEST(game_test, run_client)
+namespace ut
 {
+
+struct Message {
+    virtual ~Message(){} // we need polymorphism
+};
+    
+struct FirstMessage :  Message{
+};
+
+struct SecondMessage : Message{
+};
+
+enum Status { none, first, second };
+  
+struct Runner {
+    Status status = Status::none;
+    
+    void onFirstMessage(const FirstMessage&) { status = Status::first;}
+    void onSecondMessage(const SecondMessage&) { status =  Status::second; }
+};
+
+
+TEST(MessageHandlerTest, message_was_not_handled)
+{
+    Runner  runner;
+    const Message& msg = FirstMessage();
+    
+    MessageHandler<Message> handler;
+    bool message_was_handled = handler.handleMessage(msg);
+    
+    ASSERT_FALSE(message_was_handled);
+}
+
+TEST(MessageHandlerTest, message_was_handled)
+{
+    Runner  runner;
+    const Message& msg = FirstMessage();
+    
+    MessageHandler<Message> handler;
+    handler.registerMessage(&runner, &Runner::onFirstMessage);
+    bool message_was_handled = handler.handleMessage(msg);
+    
+    ASSERT_TRUE(message_was_handled);
+}
+
+TEST(MessageHandlerTest, execute_first)
+{
+    Runner  runner;
+    const Message& msg = FirstMessage();
+    
+    MessageHandler<Message> handler;
+    handler.registerMessage(&runner, &Runner::onFirstMessage);
+    handler.handleMessage(msg);
+    
+    ASSERT_EQ(runner.status, Status::first);
+}
+
+TEST(MessageHandlerTest, execute_second)
+{
+    Runner  runner;
+    const Message& msg = SecondMessage();
+    
+    MessageHandler<Message> handler;
+    handler.registerMessage(&runner, &Runner::onSecondMessage);
+    handler.handleMessage(msg);
+    
+    ASSERT_EQ(runner.status, Status::second);
+}
 }
