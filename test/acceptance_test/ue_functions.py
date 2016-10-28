bc7bdf0ce15eb7cc335e23e80c771b7277522177
norkos <norbert.kosmal@gmail.com>
2016-10-28 13:16:01 +0200

    Detach SCT failinig

--- test/acceptance_test/ue_functions.py
+++ test/acceptance_test/ue_functions.py
@@ -1,4 +1,6 @@
 import zmq
+import lte_pb2
+import rrc_pb2
 
 def create_ue(port):
     context = zmq.Context()
@@ -7,3 +9,23 @@ def create_ue(port):
     ue.RCVTIMEO = 1000
     
     return ue, port, port
+
+def attach_ue(ue, mme, ue_id, ue_port):    
+    mme_req = lte_pb2.ASN1()
+    mme_req.s1ap.attach_req.id = ue_id 
+    mme_req.s1ap.attach_req.port = ue_port
+    mme.send(mme_req.SerializeToString())
+    
+    message = ue.recv()
+    ue_req_ = lte_pb2.ASN1()
+    ue_req_.ParseFromString(message)
+     
+    ue_resp = lte_pb2.ASN1()
+    ue_resp.rrc.attach_resp.id = ue_id
+    ue_resp.rrc.attach_resp.status = rrc_pb2.AttachResp.OK
+    ue.send(ue_resp.SerializeToString())
+    
+    message = mme.recv()
+    mme_resp_ = lte_pb2.ASN1()
+    mme_resp_.ParseFromString(message)
+    mme_resp = mme_resp_.s1ap.attach_resp
